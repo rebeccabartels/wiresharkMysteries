c3e235eaf333241b73623e83bb6c7069d6e81343
riley <rebeccakbartels@gmail.com>
2020-01-09 20:08:10 -0500

    activity

---
+++ SniffWebAttacks/directions.sh
@@ -0,0 +1,9 @@
+Scenario
+As a Network administrator, it is your job to detect and analyze any potential breaches in your company. Recently, one of your techs noticed what they thought to be an SQL injection attack on the company website. Your technician has saved and handed over to you the pcap file for further analysis.
+Instructions
+Open the pcap in http_dvwa_sqlinjection.pcapng.
+Find the packets containing the SQLi payloads and record the three payloads you found. Hint: Use a tool like URL decoder: https://www.urldecoder.org/
+Were the attacks successful? Support your conclusion with evidence.
+Decode from URL encoded formatDecode from URL encoded format
+URL Decode and Encode - Online
+Decode from or Encode to URL encoded (also known as Percent-encoded) format - Here, with our simple online tool.(56 kB)
